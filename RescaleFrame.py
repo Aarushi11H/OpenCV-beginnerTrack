5d4ebb56c577d1ed45b1bbbfed9d8cc1e27e9744
Aarushi11H <67260600+Aarushi11H@users.noreply.github.com>
2020-07-28 11:40:14 +0530

    Add files via upload

---
+++ RescaleFrame.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+def rescale_frame(frame, percent=75):
+    width = int(frame.shape[1] * percent/ 100)
+    height = int(frame.shape[0] * percent/ 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+
+while True:
+    rect, frame = cap.read()
+    frame75 = rescale_frame(frame, percent=75)
+    cv2.imshow('frame75', frame75)
+    frame150 = rescale_frame(frame, percent=150)
+    cv2.imshow('frame150', frame150)
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
